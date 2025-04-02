c29fb8395615e798f0592eb51e25ea74226fc836
shripad naik <shripadn99@gmail.com>
2025-04-02 09:38:23 +0530

    fourth change

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import click from './Componants/click';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Welocome shripad naik</h1>
+     <click/>
+    </div>
+  );
+}
+
+export default App;
